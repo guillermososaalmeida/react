f1596be5cdc965cddef3ead84a5c0d00a6e3d288
guillebags <guillebags@gmail.com>
2024-03-07 13:47:45 +0100

    single selection and css

--- accordion/src/components/accordion/index.jsx
+++ accordion/src/components/accordion/index.jsx
@@ -3,20 +3,32 @@
 
 import { useState } from "react";
 import data from "./data";
+import "./styles.css";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
 
+  const handleSingleSelection = (currentId) => {
+    setSelected(currentId === selected ? null : currentId);
+  };
+
   return (
     <div className="wrapper">
+      <button>Enable Multiple Selection</button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
-              <div className="title">
+            <div key={dataItem.id} className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
